ff43561c7513bf143e25a35b5e972e430810bb04
Daniel Abreu <dpabreu@gmail.com>
2019-06-05 00:55:23 -0300

    Aplicando menu view no APP.

--- app/src/main/java/br/com/cds/graciedietcontrol/ui/activity/PrincipalActivity.java
+++ app/src/main/java/br/com/cds/graciedietcontrol/ui/activity/PrincipalActivity.java
@@ -1,20 +1,34 @@
 package br.com.cds.graciedietcontrol.ui.activity;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import br.com.cds.graciedietcontrol.R;
 
-public class MenusActivity extends AppCompatActivity {
+public class PrincipalActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        setTitle("GracieDiet - Control");
+        setContentView(R.layout.activity_principal);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 
     @Override
@@ -25,7 +39,6 @@ public class MenusActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
         switch (item.getItemId()) {
             case R.id.cadastro_grupo:
                 Toast.makeText(this,
@@ -43,6 +56,7 @@ public class MenusActivity extends AppCompatActivity {
                         Toast.LENGTH_SHORT).show();
                 break;
         }
+
         return super.onOptionsItemSelected(item);
     }
 }
